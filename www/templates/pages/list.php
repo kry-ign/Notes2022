b52c74a6f6f56e96fae9a7e3c66a56a1b28453bd
kry-ign <kris_ignatiuk@wp.pl>
2022-02-04 18:12:47 +0100

    commit

---
+++ www/templates/pages/list.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+</head>
+<body>
+<div class="header">
+    Moje Notatki
+</div>
+<div class="container">
+    <div class="menu">
+        <ul>
+            <li>
+                <a href="/?action=">Lista Notatek</a>
+            </li>
+            <li>
+                <a href="/?action=create">Nowa Notatka</a>
+            </li>
+        </ul>
+    </div>
+    <div>
+            Wyświetl Notatki
+    </div>
+</div>
+<div class="footer">
+</div>
+</body>
+</html>
